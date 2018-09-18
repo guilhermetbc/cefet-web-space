690d5e6c9750b973e8e5f0ab6f3db0cf44927c66
guilhermetbc <31928509+guilhermetbc@users.noreply.github.com>
2018-09-18 11:16:42 -0300

    Add files via upload

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,17 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+const plusButtons = document.querySelectorAll(".botao-expandir-retrair");
+
+plusButtons.forEach(function(plusButton){
+	plusButton.addEventListener('click', function(e){
+    	const parentParagraph = this.parentNode;
+    	if(parentParagraph.classList.length == 1){
+      		parentParagraph.classList.remove('expandido');
+      		this.innerHTML = '+';
+    	}else{
+      		parentParagraph.classList.add('expandido');
+      		this.innerHTML = '-';
+    	}
+  	});
+});
--- exercicio2.js
+++ exercicio2.js
@@ -2,10 +2,39 @@
 // Este arquivo ainda não está incluído no arquivo HTML
 
 const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
-  todasAsImagens = [
-    'philae-parts.jpg',
-    'philae-rosetta.jpg',
-    'philae-separation.jpg',
-    'philae-67-picture.jpg',
-    'philae-collecting.jpg'
-  ];
+  	todasAsImagens = [
+	    'philae-parts.jpg',
+	    'philae-rosetta.jpg',
+	    'philae-separation.jpg',
+	    'philae-67-picture.jpg',
+	    'philae-collecting.jpg'
+  	];
+
+const prevButton = document.querySelector('#anterior.controle');
+const nextButton = document.querySelector('#proximo.controle');
+const imgElement = document.querySelector('#slide');
+
+let currentImgIndex = 0;
+
+let nextImage = function(){
+  	currentImgIndex++;
+  	if(currentImgIndex >= todasAsImagens.length){
+    	currentImgIndex = 0
+  	}
+  	updateImage();  
+}
+
+let prevImage = function(){
+  	currentImgIndex--;
+  	if(currentImgIndex < 0){
+    	currentImgIndex = todasAsImagens.length - 1;
+  	}
+  	updateImage();
+}
+
+let updateImage = function(){
+  	imgElement.src = servidorDasImagens + todasAsImagens[currentImgIndex];
+}
+
+prevButton.addEventListener('click', prevImage);
+nextButton.addEventListener('click', nextImage);
